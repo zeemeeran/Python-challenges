9a123cdc0dc09440fa401c10c54b3144da62716a
Zahida Meeran <dev.zmeeran@gmail.com>
2022-05-19 03:19:26 -0400

    adding python one liner if statement code and swap code

---
+++ ifoneline.py
@@ -0,0 +1,32 @@
+
+if 1 < 2:
+    x='yes..'
+else:
+    x='no'
+#print(x)
+
+x='yes!!' if 3 < 2 else 'no!!!'
+print(x)
+
+#elif
+x = 1
+if x > 1:
+    print("no")
+elif x == 1:
+    print("yes x is 1")
+else:
+    print("maybe")
+
+
+print("no") if x > 1 else print("yes") if x == 1 else print("maybe")
+
+
+#If without else
+morning = True
+
+if morning:
+    print('Good Morning!')
+
+if morning: print('Good Morning!!')
+
+print('Good Morning...') if morning else None
--- swap.py
+++ swap.py
@@ -1,3 +1,5 @@
+# swap variables
+
 a = 10 
 b = 20
 print(a,b)
@@ -9,4 +11,20 @@ a,b,c = 10,1.7,'Hello World'
 
 print(a,b,c) 
 
+# chain conditions
+
+if 20 > a > 5:
+    print("chain conditions1 true")
+
+if 20 < a > 5:
+    print("chain condition2 true")
+else:
+    print("chain condition2 false")
+
+# function returns multiple values
+def fun():
+    return 100, True
+
+x, success = fun()
+print(f'x = {x} \nsuccess = {success}')
 
