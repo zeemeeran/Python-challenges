905705cb82faef026914529827b3001a055f9402
Zahida Meeran <dev.zmeeran@gmail.com>
2022-05-27 10:22:17 -0400

    adding code - playing with string

---
+++ strplay.py
@@ -0,0 +1,15 @@
+str1 = "cat"
+print(str1)
+str2 = list(str1)
+print(str2)
+
+for i in str1:
+    print(i)
+
+for i in range(0,len(str2)):
+    print(str2[i])
+
+for i in range(0, len(str1)):
+    print(str1[i])
+
+
